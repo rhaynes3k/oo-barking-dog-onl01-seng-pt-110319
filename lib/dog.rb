67b78a8cbc4b1feae7affce5609c01f5275c07bf
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-02 03:37:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
-  def name(name)
-    @dog_name = name
+  def name=(dog_name)
+    @this_dogs_name = dog_name
   end
-  def d_name
-    @dog_name
+ 
+  def name
+    @this_dogs_name
   end
 end
 
